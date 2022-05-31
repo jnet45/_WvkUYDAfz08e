5c1a1fe44046ea4e9dbb6972d9bcfe7d14c99869
jnet45 <81304762+jnet45@users.noreply.github.com>
2022-05-31 17:43:12 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(8)
+basic.showLeds(`
+    # # # . #
+    # . . . .
+    # . # . #
+    # . # . #
+    # # # . #
+    `)
